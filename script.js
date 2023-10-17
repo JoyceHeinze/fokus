22241e616811984c1dce1edfddd4d562fe4d31fe
Joyce Heinze <joyceheinze@ufrrj.br>
2023-10-17 18:53:58 -0300

    Inserção de documentos

---
+++ script.js
@@ -0,0 +1,124 @@
+//Página de referência(elementos visuais)
+const html = document.querySelector("html");
+const focoBt = document.querySelector(".app__card-button--foco");
+const curtoBt = document.querySelector(".app__card-button--curto");
+const longoBt = document.querySelector(".app__card-button--longo");
+const imagem = document.querySelector(".app__image");
+const titulo = document.querySelector(".app__title");
+const botoes = document.querySelectorAll(".app__card-button");
+const tempoNaTela = document.querySelector("#timer");
+
+//Sons(elementos musicais)
+const musicaInput = document.querySelector("#alternar-musica");
+const musica = new Audio("./sons/luna-rise-part-one.mp3");
+musica.loop = true;
+const musicaPausar = new Audio("./sons/pause.mp3");
+const musicaInicar = new Audio("./sons/play.wav");
+const musicaTempoFinalizado = new Audio("./sons/beep.mp3");
+
+
+//Botão de começar e temporizador;
+const startPauseBt = document.querySelector("#start-pause");
+const textoStartPauseBt = document.querySelector("#start-pause span");
+const imagemStartPauseBt = document.querySelector(".app__card-primary-butto-icon");
+let tempoDecorridoEmSegundos = 1500;
+let intervaloId = null;
+
+musicaInput.addEventListener("change", () => {
+    if(musica.paused) {
+        musica.play();
+    } else {
+        musica.pause();
+    }
+})
+
+focoBt.addEventListener("click", () => {
+    tempoDecorridoEmSegundos = 1500;
+    alterarContexto("foco");
+    focoBt.classList.add("active");
+})
+
+curtoBt.addEventListener("click", () => {
+    tempoDecorridoEmSegundos = 300;
+    alterarContexto("descanso-curto");
+    curtoBt.classList.add("active");
+})
+
+longoBt.addEventListener("click", () => {
+    tempoDecorridoEmSegundos = 900;
+    alterarContexto("descanso-longo");
+    longoBt.classList.add("active");
+})
+
+function alterarContexto(contexto) {
+    mostrarTempo();
+    botoes.forEach(function(contexto) {
+        contexto.classList.remove("active");
+    })
+
+    html.setAttribute("data-contexto", contexto);
+    imagem.setAttribute("src", `./imagens/${contexto}.png`);
+    
+    switch (contexto) {
+        case "foco":
+            titulo.innerHTML = `
+        Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>
+        `
+            break;
+        case "descanso-curto":
+            titulo.innerHTML = `
+        Que tal dar uma respirada?<br>
+                <strong class="app__title-strong">Faça uma pausa curta!</strong>
+        `
+            break;
+        case "descanso-longo":
+            titulo.innerHTML = `
+        Hora de voltar à superfície.<br>
+                <strong class="app__title-strong">Faça uma pausa longa.</strong>
+        `
+            break;
+        default:
+            break;
+    }
+}
+
+const contagemRegressiva = () => {
+    if(tempoDecorridoEmSegundos <=0) {
+        musicaTempoFinalizado.play();
+        alert("Tempo Finalizado!");
+        zerar();
+        return
+    }
+    tempoDecorridoEmSegundos -= 1;
+    mostrarTempo();
+}
+
+startPauseBt.addEventListener("click", iniciarOuPausar);
+
+function iniciarOuPausar() {
+    if(intervaloId){
+        musicaPausar.play();
+        zerar();
+        return
+    }
+    musicaInicar.play();
+    intervaloId = setInterval(contagemRegressiva, 1000);
+    textoStartPauseBt.textContent = "Pausar";
+    imagemStartPauseBt.setAttribute("src", "./imagens/pause.png");
+}
+
+function zerar() {
+    clearInterval(intervaloId);
+    textoStartPauseBt.textContent = "Começar";
+    imagemStartPauseBt.setAttribute("src", "./imagens/play_arrow.png");
+    intervaloId = null;
+}
+
+function mostrarTempo() {
+    const tempo = new Date(tempoDecorridoEmSegundos * 1000);
+    const tempoFormatado = tempo.toLocaleTimeString("pt-Br", {minute: "2-digit", second: "2-digit"});
+    tempoNaTela.innerHTML = `${tempoFormatado}`
+}
+
+mostrarTempo();
